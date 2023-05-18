c6d5a3f7bd2bda50f8e5c49ff76ba5a8e70f8942
GalenRhodes <galenrhodes@me.com>
2023-05-18 08:02:40 -0400

    Initial commit

---
+++ src/main/java/com/projectgalen/lib/ui/progressdialog/ProgressDialog.java
@@ -0,0 +1,248 @@
+package com.projectgalen.lib.ui.progressdialog;
+
+import com.projectgalen.lib.ui.ButtonChoice;
+import com.projectgalen.lib.ui.errors.ProgressDialogException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@SuppressWarnings("unused")
+public class ProgressDialog extends JDialog {
+
+    protected JPanel       contentPane;
+    protected JButton      buttonOK;
+    protected JProgressBar progressBar;
+    protected JLabel       message;
+    protected ButtonChoice buttonChoice = ButtonChoice.None;
+    protected JLabel       finalMessageLabel;
+
+    public ProgressDialog(JFrame frame, @NotNull String title, @NotNull String message, int minValue, int maxValue, int initialValue) {
+        super(frame, title, true);
+        setup(message, minValue, maxValue, initialValue);
+    }
+
+    public ProgressDialog(JDialog dialog, @NotNull String title, @NotNull String message, int minValue, int maxValue, int initialValue) {
+        super(dialog, title, true);
+        setup(message, minValue, maxValue, initialValue);
+    }
+
+    public ButtonChoice getButtonChoice() {
+        return buttonChoice;
+    }
+
+    public @NotNull String getMessage() {
+        return message.getText();
+    }
+
+    public int getProgress() {
+        return progressBar.getValue();
+    }
+
+    public int getProgressMax() {
+        return progressBar.getMaximum();
+    }
+
+    public int getProgressMin() {
+        return progressBar.getMinimum();
+    }
+
+    public String getProgressText() {
+        return progressBar.getString();
+    }
+
+    public void setMessage(@NotNull String msg) {
+        SwingUtilities.invokeLater(() -> message.setText(msg));
+    }
+
+    public void setProgress(int value) {
+        SwingUtilities.invokeLater(() -> progressBar.setValue(value));
+    }
+
+    public void setProgressMax(int maxValue) {
+        SwingUtilities.invokeLater(() -> progressBar.setMaximum(maxValue));
+    }
+
+    public void setProgressMin(int minValue) {
+        SwingUtilities.invokeLater(() -> progressBar.setMinimum(minValue));
+    }
+
+    public void setProgressText(@NotNull String text) {
+        SwingUtilities.invokeLater(() -> progressBar.setString(text));
+    }
+
+    private void onOK() {
+        buttonChoice = ButtonChoice.OK;
+        dispose();
+    }
+
+    private void setFailedMessage(@NotNull String err) {
+        SwingUtilities.invokeLater(() -> {
+            finalMessageLabel.setForeground(Color.red);
+            finalMessageLabel.setText(String.format("An Error Occurred: %s", err));
+        });
+    }
+
+    private void setSuccessMessage() {
+        SwingUtilities.invokeLater(() -> {
+            finalMessageLabel.setForeground(new Color(0, 128, 0));
+            finalMessageLabel.setText("Success!");
+        });
+    }
+
+    private void setup(@NotNull String message, int minValue, int maxValue, int initialValue) {
+        setContentPane(contentPane);
+        getRootPane().setDefaultButton(buttonOK);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        buttonOK.addActionListener(e -> onOK());
+        progressBar.setMinimum(minValue);
+        progressBar.setMaximum(maxValue);
+        progressBar.setValue(initialValue);
+        boolean indeterminate = ((minValue == 0) && (maxValue == 0));
+        progressBar.setIndeterminate(indeterminate);
+        progressBar.setString(indeterminate ? "..." : null);
+        this.message.setText(message);
+        progressBar.setStringPainted(true);
+        progressBar.setMinimumSize(new Dimension(progressBar.getMinimumSize().width, (int)(progressBar.getMinimumSize().height * 1.5)));
+        finalMessageLabel.setText(" ");
+        pack();
+        setMinimumSize(new Dimension(400, getSize().height));
+        pack();
+    }
+
+    public static void execute(Component owner, @NotNull String title, @NotNull ProgressDialog.ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, "", 0, 100, 0, false, lambda);
+    }
+
+    public static void execute(Component owner, @NotNull String title, boolean autoOK, @NotNull ProgressDialog.ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, "", 0, 100, 0, autoOK, lambda);
+    }
+
+    public static void execute(Component owner, @NotNull String title, @NotNull String message, @NotNull ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, message, 0, 100, 0, false, lambda);
+    }
+
+    public static void execute(Component owner, @NotNull String title, @NotNull String message, boolean autoOK, @NotNull ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, message, 0, 100, 0, autoOK, lambda);
+    }
+
+    public static void execute(Component owner, @NotNull String title, int minValue, int maxValue, int initialValue, @NotNull ProgressDialog.ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, "", minValue, maxValue, initialValue, false, lambda);
+    }
+
+    public static void execute(Component owner, @NotNull String title, int minValue, int maxValue, int initialValue, boolean autoOK, @NotNull ProgressDialog.ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, "", minValue, maxValue, initialValue, autoOK, lambda);
+    }
+
+    public static void execute(Component owner,
+                               @NotNull String title,
+                               @NotNull String message,
+                               int minValue,
+                               int maxValue,
+                               int initialValue,
+                               @NotNull ProgressDialog.ProgressExecuteDialogLambda lambda) {
+        execute(owner, title, message, minValue, maxValue, initialValue, false, lambda);
+    }
+
+    public static void execute(Component owner,
+                               @NotNull String title,
+                               @NotNull String message,
+                               int minValue,
+                               int maxValue,
+                               int initialValue,
+                               boolean autoOK,
+                               @NotNull ProgressDialog.ProgressExecuteDialogLambda lambda) {
+        fetch(owner, title, message, minValue, maxValue, initialValue, autoOK, dlg -> {
+            lambda.action(dlg);
+            return null;
+        });
+    }
+
+    public static <T> T fetch(Component owner, @NotNull String title, int minValue, int maxValue, int initialValue, boolean autoOK, @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, "", minValue, maxValue, initialValue, autoOK, lambda);
+    }
+
+    public static <T> T fetch(Component owner, @NotNull String title, @NotNull String message, boolean autoOK, @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, message, 0, 100, 0, autoOK, lambda);
+    }
+
+    public static <T> T fetch(Component owner, @NotNull String title, boolean autoOK, @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, "", 0, 100, 0, autoOK, lambda);
+    }
+
+    public static <T> T fetch(Component owner, @NotNull String title, int minValue, int maxValue, int initialValue, @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, "", minValue, maxValue, initialValue, false, lambda);
+    }
+
+    public static <T> T fetch(Component owner, @NotNull String title, @NotNull String message, @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, message, 0, 100, 0, false, lambda);
+    }
+
+    public static <T> T fetch(Component owner, @NotNull String title, @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, "", 0, 100, 0, false, lambda);
+    }
+
+    public static <T> T fetch(Component owner,
+                              @NotNull String title,
+                              @NotNull String message,
+                              int minValue,
+                              int maxValue,
+                              int initialValue,
+                              @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        return fetch(owner, title, message, minValue, maxValue, initialValue, false, lambda);
+    }
+
+    public static <T> T fetch(Component owner,
+                              @NotNull String title,
+                              @NotNull String message,
+                              int minValue,
+                              int maxValue,
+                              int initialValue,
+                              boolean autoOK,
+                              @NotNull ProgressDialog.ProgressFetchDialogLambda<T> lambda) {
+        ProgressDialog dlg = create(owner, title, message, minValue, maxValue, initialValue);
+        dlg.message.setText(message);
+        dlg.pack();
+        dlg.setLocationRelativeTo(owner);
+        dlg.setResizable(false);
+
+        Future<T> future = Executors.newSingleThreadExecutor().submit(() -> {
+            SwingUtilities.invokeLater(() -> dlg.buttonOK.setEnabled(false));
+            try {
+                T results = lambda.action(dlg);
+                dlg.setSuccessMessage();
+                return results;
+            }
+            catch(Exception e) {
+                dlg.setFailedMessage(e.toString());
+                throw e;
+            }
+            finally {
+                SwingUtilities.invokeLater(() -> dlg.buttonOK.setEnabled(true));
+                if(autoOK) SwingUtilities.invokeLater(dlg::onOK);
+            }
+        });
+
+        dlg.setVisible(true);
+        try { return future.get(); } catch(Exception e) { throw new ProgressDialogException(e); }
+    }
+
+    @Contract("null, _, _, _, _, _ -> new")
+    private static @NotNull ProgressDialog create(Component owner, @NotNull String title, @NotNull String message, int minValue, int maxValue, int initialValue) {
+        if(owner instanceof JDialog) return new ProgressDialog((JDialog)owner, title, message, minValue, maxValue, initialValue);
+        if(owner instanceof JFrame) return new ProgressDialog((JFrame)owner, title, message, minValue, maxValue, initialValue);
+        return new ProgressDialog((JFrame)null, title, message, minValue, maxValue, initialValue);
+    }
+
+    public interface ProgressExecuteDialogLambda {
+        void action(@NotNull ProgressDialog progressDialog) throws Exception;
+    }
+
+    public interface ProgressFetchDialogLambda<T> {
+        @Nullable T action(@NotNull ProgressDialog progressDialog) throws Exception;
+    }
+}
